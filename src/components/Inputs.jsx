023128f533843feb220c226e70103226b91f0ac8
Nerea Battauz <nereebattauz@gmail.com>
2024-12-16 18:20:53 -0300

    local storage y manejo del botón

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { FaPaperPlane } from "react-icons/fa";
 import { setLocalStorage } from "../localStorage";
@@ -6,75 +6,104 @@ import { getLocalStorage } from "../localStorage";
 //import List from "./List";
 
 const Inputs = ({ FormControl, FormLabel, Input, Select }) => {
-
-//Guardar tarea ingresada en el input en Local storage
-  const [inputValue, setInputValue] = useState("")
+  //Guardar tarea ingresada en el input en Local storage
+  const [inputValue, setInputValue] = useState("");
+  const [tasksArray, setTasksArray] = useState([]);
   const changeInputValue = (e) => {
-    setInputValue(e.target.value)
-    console.log(inputValue)
-    localStorage.setItem("Tarea", inputValue)
-  }
+    setInputValue(e.target.value);
+    console.log(inputValue);
+    localStorage.setItem("Tarea", inputValue);
+  };
 
-//Guardar valor seleccionado del select en Local storage
-  const [selectValue, setSelectValue] = useState("")
-const changeValue = (e) => {
-  setSelectValue(e.target.value)
-  console.log(e.target.value);
-}
+  //Guardar valor seleccionado del select en Local storage
+  const [selectValue, setSelectValue] = useState("");
+  const changeValue = (e) => {
+    setSelectValue(e.target.value);
+    //console.log(e.target.value);
+  };
 
-//Agregar tarea a la lista de tarea
-const addTask = () => {
-  console.log(localStorage.getItem("Tarea"));
-};
+  //Agregar tarea a la lista de tarea
+  const addTask = (e) => {
+    e.preventDefault();
+    if (localStorage.getItem("Tarea") === "") {
+      console.log("Ingrese una tarea");
+    } else {
+      setTasksArray([...tasksArray, inputValue]);
+      setInputValue("");
+      localStorage.setItem("Tarea", "")
+      console.log(inputValue);
+    }
+  };
+  useEffect(() => {
+    
+  }, [inputValue]);
 
-//DEVOLUCIÓN HTML
-return (
-//Contenedor principal
-  <Box display="flex" flexDir="column" gap="8" w="80%">
-    <Box
-      display="flex"
-      gap="10"
-      flexDir={{ base: "column", md: "row" }}
-      justifySelf="center"
-    >
+  useEffect(() => {
+    console.log("Tareas actualizadas:", tasksArray);
+  }, [tasksArray]);
 
-{/* Input text tarea */}
-      <FormControl>
-    <FormLabel>Tarea</FormLabel>
-    <Input
-    value={inputValue}
-      size="lg"
-      bg="white"
-      color="gray.600"
-      placeholder="Ingresar una tarea"
-      onChange={changeInputValue}
-    />
-  </FormControl>
+  //DEVOLUCIÓN HTML
+  return (
+    //Contenedor principal
+    <Box display="flex" flexDir="column" gap="8" w="80%">
+      <Box
+        display="flex"
+        gap="10"
+        flexDir={{ base: "column", md: "row" }}
+        justifySelf="center"
+      >
+        {/* Input text tarea */}
+        <FormControl>
+          <FormLabel>Tarea</FormLabel>
+          <Input
+            value={inputValue}
+            size="lg"
+            bg="white"
+            color="gray.600"
+            placeholder="Ingresar una tarea"
+            onChange={changeInputValue}
+          />
+        </FormControl>
 
-{/* Input select estado tarea */}
-  <FormControl>
-        <FormLabel>Seleccionar</FormLabel>
-        <Select value={selectValue} size="lg" bg="white" color="gray.600" onChange={changeValue}>
-          <option color="purple.800" value="all">
-            Todas las tareas
-          </option>
-          <option color="purple.800" value="incomplete">
-            Tareas incompletas
-          </option>
-          <option color="purple.800" value="completed">
-            Tareas completadas
-          </option>
-        </Select>
-      </FormControl>
+        {/* Input select estado tarea */}
+        <FormControl>
+          <FormLabel>Seleccionar</FormLabel>
+          <Select
+            value={selectValue}
+            size="lg"
+            bg="white"
+            color="gray.600"
+            onChange={changeValue}
+          >
+            <option color="purple.800" value="all">
+              Todas las tareas
+            </option>
+            <option color="purple.800" value="incomplete">
+              Tareas incompletas
+            </option>
+            <option color="purple.800" value="completed">
+              Tareas completadas
+            </option>
+          </Select>
+        </FormControl>
+      </Box>
+      {/* Botón agregar tarea */}
+      <Button
+        colorScheme="purple"
+        gap="4"
+        size="lg"
+        onClick={addTask}
+        type="submit"
+      >
+        Agregar tarea
+        <FaPaperPlane />
+      </Button>
+      {/* <div>
+        {tasksArray.map((task) => {
+          <Text>{task}</Text>;
+        })}
+      </div> */}
     </Box>
-
-{/* Botón agregar tarea */}
-    <Button colorScheme="purple" gap="4" size="lg" onClick={addTask}>
-      Agregar tarea
-      <FaPaperPlane />
-    </Button>{" "}
-  </Box>
-);
+  );
 };
-
 export default Inputs;
